582480ef60dbe79cf2998d154fb5d4dbd4a323bc
Alfurqon02 <muhammadalfurqon61@gmail.com>
2024-02-25 19:34:03 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>Project buat latihan, <a href="https://www.freecodecamp.org/news/how-to-update-node-and-npm-to-the-latest-version/">referensi</a></h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/TodoMainView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ToDoItem from '@/components/ToDoItem.vue'
+
+</script>
+
+<template>
+  <main>
+    <ToDoItem 
+    />
+  </main>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
