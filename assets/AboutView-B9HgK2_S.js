77bd81dbd64fbfc3d5f75da006a8dac426913a7a
Alfurqon02 <muhammadalfurqon61@gmail.com>
2024-11-23 02:28:52 +0700

    Updates

---
+++ assets/AboutView-B9HgK2_S.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,o,a as e,b as s}from"./index-Br-ombxO.js";const c={},n={class:"about"},r=e("h1",null,[s("Project buat latihan, "),e("a",{href:"https://medium.com/@ibnusyawall/membuat-aplikasi-todo-list-dengan-vue-3-pwa-a20c1e29c326"},"referensi")],-1),i=[r];function _(d,l){return o(),a("div",n,i)}const m=t(c,[["render",_]]);export{m as default};
